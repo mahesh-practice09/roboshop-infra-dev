74919d7c5c55355aef6eec50f3053cad91f05c65
mahesh.ganji <mahesh050917@gmail.com>
2026-04-15 23:47:55 +0530

    terraform code updated

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -21,3 +21,33 @@ data "aws_ssm_parameter" "user_sg_id" {
   
   name = "/${var.Project}/${var.Env}/user_sg_id"
 }
+
+data "aws_ssm_parameter" "redis_sg_id" {
+  
+  name = "/${var.Project}/${var.Env}/redis_sg_id"
+}
+
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
+  
+  name = "/${var.Project}/${var.Env}/rabbitmq_sg_id"
+}
+
+data "aws_ssm_parameter" "mysql_sg_id" {
+  
+  name = "/${var.Project}/${var.Env}/mysql_sg_id"
+}
+
+data "aws_ssm_parameter" "cart_sg_id" {
+  
+  name = "/${var.Project}/${var.Env}/cart_sg_id"
+}
+
+data "aws_ssm_parameter" "shipping_sg_id" {
+  
+  name = "/${var.Project}/${var.Env}/shipping_sg_id"
+}
+
+data "aws_ssm_parameter" "payment_sg_id" {
+  
+  name = "/${var.Project}/${var.Env}/payment_sg_id"
+}
--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -33,3 +33,66 @@ resource "aws_security_group_rule" "user_mongodb" {
   source_security_group_id = data.aws_ssm_parameter.user_sg_id.value
   security_group_id = data.aws_ssm_parameter.mongodb_sg_id.value
 }
+
+resource "aws_security_group_rule" "bastion_redis" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = data.aws_ssm_parameter.bastion_sg_id.value
+  security_group_id = data.aws_ssm_parameter.redis_sg_id.value
+}
+
+resource "aws_security_group_rule" "user_redis" {
+  type              = "ingress"
+  from_port         = 6379
+  to_port           = 6379
+  protocol          = "tcp"
+  source_security_group_id = data.aws_ssm_parameter.user_sg_id.value
+  security_group_id = data.aws_ssm_parameter.redis_sg_id.value
+}
+
+resource "aws_security_group_rule" "cart_redis" {
+  type              = "ingress"
+  from_port         = 6379
+  to_port           = 6379
+  protocol          = "tcp"
+  source_security_group_id = data.aws_ssm_parameter.cart_sg_id.value
+  security_group_id = data.aws_ssm_parameter.redis_sg_id.value
+}
+
+resource "aws_security_group_rule" "bastion_mysql" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = data.aws_ssm_parameter.bastion_sg_id.value
+  security_group_id = data.aws_ssm_parameter.mysql_sg_id.value
+}
+
+resource "aws_security_group_rule" "shipping_mysql" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id = data.aws_ssm_parameter.shipping_sg_id.value
+  security_group_id = data.aws_ssm_parameter.mysql_sg_id.value
+}
+
+resource "aws_security_group_rule" "bastion_rabbitmq" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = data.aws_ssm_parameter.bastion_sg_id.value
+  security_group_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
+}
+
+resource "aws_security_group_rule" "payment_rabbitmq" {
+  type              = "ingress"
+  from_port         = 5672
+  to_port           = 5672
+  protocol          = "tcp"
+  source_security_group_id = data.aws_ssm_parameter.payment_sg_id.value
+  security_group_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
+}
