b242e1517a4b13255b1ec1ac541eec8842d150a1
mahesh.ganji <mahesh050917@gmail.com>
2026-04-15 21:41:03 +0530

    terraform code updated

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 component=$1
-
-sudo ansible-playbook -e component=mongodb roboshop.yaml
+git clone https://github.com/mahesh-practice09/ansible-roboshop-roles-tf.git
+cd ansible-roboshop-roles-tf
+sudo ansible-playbook -e component=$1 roboshop.yaml
