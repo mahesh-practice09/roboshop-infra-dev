c70593bbd948d3dd7e0fdcd3ed1354f15fe5dd1e
mahesh.ganji <mahesh050917@gmail.com>
2026-04-16 17:46:44 +0530

    terraform code updated

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo dnf install ansible -y
+ansible-galaxy collection install amazon.aws:6.0.1 --force
 component=$1
 rm -rf ansible-roboshop-roles-tf
 git clone https://github.com/mahesh-practice09/ansible-roboshop-roles-tf.git
