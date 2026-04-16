7eb871b00f55f6eedaf530e1b3a28b7e37765f3f
mahesh.ganji <mahesh050917@gmail.com>
2026-04-16 18:46:25 +0530

    ansible code updated

--- 40-databases/iam.tf
+++ 40-databases/iam.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "mysqlrole" {
+resource "aws_iam_role" "mysql" {
   name = local.mysql_role_name
 
   assume_role_policy = jsonencode({
@@ -16,18 +16,18 @@ resource "aws_iam_role" "mysqlrole" {
    tags = merge(local.common_tags ,{Name = local.mysql_role_name})
 }
 
-resource "aws_iam_policy" "sqlpolicy" {
+resource "aws_iam_policy" "sql" {
   name        = local.mysql_role_policy
   
    policy = file("mysql-iam-policy.json")
 }
 
-resource "aws_iam_role_policy_attachment" "mysql_attach_policy" {
+resource "aws_iam_role_policy_attachment" "mysql" {
   role = aws_iam_role.mysqlrole.name
   policy_arn = aws_iam_policy.sqlpolicy.arn
 }
 
-resource "aws_iam_instance_profile" "mysqlprofile" {
+resource "aws_iam_instance_profile" "mysql" {
    name =  "${var.Project}-${var.Env}-mysql-profile"
    role = aws_iam_role.mysqlrole.name
 }
