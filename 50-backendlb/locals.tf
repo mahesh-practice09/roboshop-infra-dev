1d5f47934a28007dde322c5217e2734b0c9e8c88
mahesh <mahesh050917@gmail.com>
2026-04-24 11:30:54 +0530

    updated terraform code

--- 50-backendlb/locals.tf
+++ 50-backendlb/locals.tf
@@ -1,6 +1,6 @@
 locals {
     private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
-    frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
+    backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
     common_tags = {
         Project = var.Project
         Terraform = true
