0a2e49b2810918754b9bf87b150be816b7596fbc
mahesh <mahesh050917@gmail.com>
2026-04-24 12:28:22 +0530

    updated terraform code

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -206,3 +206,12 @@ resource "aws_security_group_rule" "internet_frontendalb" {
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
 }
+
+resource "aws_security_group_rule" "bastion_frontend" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = data.aws_ssm_parameter.bastion_sg_id.value
+  security_group_id = data.aws_ssm_parameter.frontend_sg_id.value
+}
