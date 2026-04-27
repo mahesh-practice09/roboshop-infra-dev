5d5db5f91637e297dfcba2aaf621e81a33eb1b02
mahesh <mahesh050917@gmail.com>
2026-04-27 09:12:41 +0530

    terraform code updated

--- 90-components/data.tf
+++ 90-components/data.tf
@@ -26,55 +26,55 @@ data "aws_ssm_parameter" "backendalb_listener_arn" {
 }
 
 
-# data "aws_ssm_parameter" "bastion_sg_id" {
+data "aws_ssm_parameter" "bastion_sg_id" {
   
-#   name = "/${var.Project}/${var.environment}/bastion_sg_id"
-# }
+  name = "/${var.Project}/${var.environment}/bastion_sg_id"
+}
 
-# data "aws_ssm_parameter" "mongodb_sg_id" {
+data "aws_ssm_parameter" "mongodb_sg_id" {
   
-#   name = "/${var.Project}/${var.environment}/mongodb_sg_id"
-# }
+  name = "/${var.Project}/${var.environment}/mongodb_sg_id"
+}
 
-# data "aws_ssm_parameter" "user_sg_id" {
+data "aws_ssm_parameter" "user_sg_id" {
   
-#   name = "/${var.Project}/${var.environment}/user_sg_id"
-# }
+  name = "/${var.Project}/${var.environment}/user_sg_id"
+}
 
-# data "aws_ssm_parameter" "redis_sg_id" {
+data "aws_ssm_parameter" "redis_sg_id" {
   
-#   name = "/${var.Project}/${var.environment}/redis_sg_id"
-# }
+  name = "/${var.Project}/${var.environment}/redis_sg_id"
+}
 
-# data "aws_ssm_parameter" "rabbitmq_sg_id" {
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
   
-#   name = "/${var.Project}/${var.environment}/rabbitmq_sg_id"
-# }
+  name = "/${var.Project}/${var.environment}/rabbitmq_sg_id"
+}
 
-# data "aws_ssm_parameter" "mysql_sg_id" {
+data "aws_ssm_parameter" "mysql_sg_id" {
   
-#   name = "/${var.Project}/${var.environment}/mysql_sg_id"
-# }
+  name = "/${var.Project}/${var.environment}/mysql_sg_id"
+}
 
-# data "aws_ssm_parameter" "cart_sg_id" {
+data "aws_ssm_parameter" "cart_sg_id" {
   
-#   name = "/${var.Project}/${var.environment}/cart_sg_id"
-# }
+  name = "/${var.Project}/${var.environment}/cart_sg_id"
+}
 
-# data "aws_ssm_parameter" "shipping_sg_id" {
+data "aws_ssm_parameter" "shipping_sg_id" {
   
-#   name = "/${var.Project}/${var.environment}/shipping_sg_id"
-# }
+  name = "/${var.Project}/${var.environment}/shipping_sg_id"
+}
 
-# data "aws_ssm_parameter" "payment_sg_id" {
+data "aws_ssm_parameter" "payment_sg_id" {
   
-#   name = "/${var.Project}/${var.environment}/payment_sg_id"
-# }
+  name = "/${var.Project}/${var.environment}/payment_sg_id"
+}
 
-# data "aws_ssm_parameter" "backend_alb_sg_id" {
+data "aws_ssm_parameter" "backend_alb_sg_id" {
   
-#   name = "/${var.Project}/${var.environment}/backend_alb_sg_id"
-# }
+  name = "/${var.Project}/${var.environment}/backend_alb_sg_id"
+}
 
 
 
